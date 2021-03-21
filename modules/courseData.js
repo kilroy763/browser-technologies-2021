3b12ea3d38f50db2f5ecf11c988101b33e842e6c
kilroy763 <itzmaxkiljan@hotmail.com>
2021-03-21 13:07:32 +0100

    code works

--- modules/courseData.js
+++ modules/courseData.js
@@ -2,7 +2,7 @@ module.exports = function getData(param) {
     let data;
     if (param === 'PWA') {
         data = {
-            lecturer: ["PPK", "Koop"]
+            lecturer: ["PPK", "Koop", "suus"]
         }
         return data;
     } else if (param === 'browser-technologies') {
