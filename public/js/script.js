5128f1278e1391bef4e1c595c2e91a4b95119569
kilroy763 <itzmaxkiljan@hotmail.com>
2021-03-24 15:35:18 +0100

    safari test

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+var een = document.getElementById('een')
+var twee = document.getElementById('twee')
+var drie = document.getElementById('drie')
+var vier = document.getElementById('vier')
+var vijf = document.getElementById('vijf')
+var zes = document.getElementById('zes')
+
+
