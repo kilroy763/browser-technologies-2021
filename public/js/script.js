9db4a34ec9b831ac3480ae0770d026a5757c36e0
kilroy763 <itzmaxkiljan@hotmail.com>
2021-03-26 11:03:43 +0100

    images added

--- public/js/script.js
+++ public/js/script.js
@@ -1,94 +1,80 @@
-
-
 console.log("het werkt")
 
 
 
-window.onload = function onzichtbaar(){
+window.onload = function onzichtbaar() {
 
-    document.getElementById("een").style.display = "none";
-    document.getElementById("twee").style.display = "none";
-    document.getElementById("drie").style.display = "none";
-    document.getElementById("vier").style.display = "none";
-    document.getElementById("vijf").style.display = "none";
-    document.getElementById("zes").style.display = "none";
-
-
-
-    
-
-    document.getElementById("submitEnquete").disabled = true;
-    document.querySelector('input[name="lecturer"]').onclick = toggleCheckbox;
-
-    function toggleCheckbox() {
-        var lfckv = document.querySelector('input[name="lecturer"]').checked;
-        if (lfckv) {
-          document.getElementById("een").style.display = "block";
-
-        } else {
-          document.getElementById("een").style.display = "none";
-        }
-      }
-
-      document.querySelector('input[name="period"]').onclick = toggleCheckbox2;
-
-      function toggleCheckbox2() {
-          var lfckv = document.querySelector('input[name="period"]').checked;
-          if (lfckv) {
-            document.getElementById("twee").style.display = "block";
-          } else {
-            document.getElementById("twee").style.display = "none";
-          }
-        }
-
-        document.querySelector('input[name="grade"]').onclick = toggleCheckbox3;
-
-        function toggleCheckbox3() {
-            var lfckv = document.querySelector('input[name="grade"]').checked;
-            if (lfckv) {
-              document.getElementById("drie").style.display = "block";
-            } else {
-              document.getElementById("drie").style.display = "none";
-            }
-          }
-
-          document.querySelector('input[name="material"]').onclick = toggleCheckbox4;
-
-          function toggleCheckbox4() {
-              var lfckv = document.querySelector('input[name="material"]').checked;
-              if (lfckv) {
-                document.getElementById("vier").style.display = "block";
-              } else {
-                document.getElementById("vier").style.display = "none";
-              }
-            }
+  document.getElementById("een").style.display = "none";
+  document.getElementById("twee").style.display = "none";
+  document.getElementById("drie").style.display = "none";
+  document.getElementById("vier").style.display = "none";
+  document.getElementById("vijf").style.display = "none";
+  document.getElementById("zes").style.display = "none";
 
 
-          document.querySelector('input[name="content"]').onclick = toggleCheckbox5;
 
-          function toggleCheckbox5() {
-              var lfckv = document.querySelector('input[name="content"]').checked;
-              if (lfckv) {
-                document.getElementById("vijf").style.display = "block";
-              } else {
-                document.getElementById("vijf").style.display = "none";
-              }
-            }
 
-        
 
-            document.querySelector('input[name="learning"]').onclick = toggleCheckbox6;
+  document.getElementById("submitEnquete").disabled = true;
 
-            function toggleCheckbox6() {
-                var lfckv = document.querySelector('input[name="learning"]').checked;
-                if (lfckv) {
-                  document.getElementById("zes").style.display = "block";
-                  document.getElementById("submitEnquete").disabled = false;
-                } else {
-                  document.getElementById("zes").style.display = "none";
-                }
-              }
+  var lecturerRadioButtons = document.querySelectorAll('input[name="lecturer"]');
+  lecturerRadioButtons.forEach(function (lecturerRadioButton) {
+    lecturerRadioButton.addEventListener("change", toggleCheckbox)
+  })
+
+  function toggleCheckbox() {
+    document.getElementById("een").style.display = "block";
+    console.log(lecturerRadioButtons)
+  }
+
+
+  var periodRadioButtons = document.querySelectorAll('input[name="period"]');
+  periodRadioButtons.forEach(function (periodRadioButton) {
+    periodRadioButton.addEventListener("change", toggleCheckbox2)
+  })
+
+  function toggleCheckbox2() {
+    document.getElementById("twee").style.display = "block";
+  }
+
+
+  var gradeRadioButtons = document.querySelectorAll('input[name="grade"]');
+  gradeRadioButtons.forEach(function (gradeRadioButton) {
+    gradeRadioButton.addEventListener("change", toggleCheckbox3)
+  })
+
+
+  function toggleCheckbox3() {
+    document.getElementById("drie").style.display = "block";
+  }
+
+
+  var materialRadioButtons = document.querySelectorAll('input[name="material"]');
+  materialRadioButtons.forEach(function (materialRadioButton) {
+    materialRadioButton.addEventListener("change", toggleCheckbox4)
+  })
+
+  function toggleCheckbox4() {
+    document.getElementById("vier").style.display = "block";
+  }
+
+  var contentRadioButtons = document.querySelectorAll('input[name="content"]');
+  contentRadioButtons.forEach(function (contentRadioButton) {
+    contentRadioButton.addEventListener("change", toggleCheckbox5)
+  })
+
+  function toggleCheckbox5() {
+    document.getElementById("vijf").style.display = "block";
+  }
+
+  var learningRadioButtons = document.querySelectorAll('input[name="learning"]');
+  learningRadioButtons.forEach(function (learningRadioButton) {
+    learningRadioButton.addEventListener("change", toggleCheckbox6)
+  })
+
+  function toggleCheckbox6() {
+    document.getElementById("zes").style.display = "block";
+    document.getElementById("submitEnquete").disabled = false;
+  }
 
 }
-
-
